51dc44b8feacebd3b94d7c054cf5492110322579
AdamBougher <a.bougher1999@gmail.com>
2025-12-02 23:11:01 -0600

    added sea monster

---
+++ back-end/src/main/java/game/interfaces/monsterStratigy.java
@@ -0,0 +1,7 @@
+package game.interfaces;
+
+import java.awt.Point;
+
+public interface monsterStratigy {
+    public Direction Move(Point monsterPosition);
+}
--- back-end/src/main/java/game/seaMonster.java
+++ back-end/src/main/java/game/seaMonster.java
@@ -1,6 +1,34 @@
 package game;
 
-public class seaMonster extends Enemy {
+import java.awt.Point;
+import java.util.ArrayList;
+import java.util.Observable;
+import java.util.Observer;
+
+import game.interfaces.monsterStratigy;
+
+public class seaMonster extends Enemy implements Observer {
+
+    private monsterStratigy strategy;
+
+    public seaMonster(int id, Point startPosition, monsterStratigy strategy, Board board) {
+        this.ID = id;
+        this.position = startPosition;
+        this.strategy = strategy;
+        this.history = new ArrayList<>();
+        this.board = board;
+    }
+
+    @Override      
+    public void update(Observable o, Object arg) {
+        board.clearCell(position);
+
+        Move(strategy.Move(this.position));
+
+        board.setCell(position, cellState.SEA_MONSTER);
+
+        history.add(board.getCell(position.x, position.y));
+    }
 
 
 }
